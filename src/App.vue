b1d20f9eff04f126ceb27fcc99eff8f1f871444a
oljakostiv <oljakostivv@gmail.com>
2021-10-06 04:12:30 +0300

    new work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <h1>Hello everyone from {{ name }}!</h1>
+
+    <button class="btnApp" @click="changeName">Change name</button>
+    <Button :text="`Awesome`" @button-click="changeName" />
+    <br />
+    <img :src="img" :alt="`Alt is ${alt}`" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Button from "@/components/Button";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { Button },
+  data() {
+    return {
+      name: "Olha",
+      img: "https://memegenerator.net/img/instances/65894975.jpg",
+      alt: "meme"
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = Math.floor(Math.random() * 1000);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
+  background: #e9eaeb;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
   margin-top: 60px;
+}
+
+.btnApp {
+  background: #97781c;
+  border: 2px solid #2c3e50;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #fff;
+  transition: 0.2s;
+  cursor: pointer;
+
+  padding: 5px 8px;
+  margin: 5px;
+}
+
+.btnApp:hover {
+  transform: scale(1.03, 1.03);
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button class="btn" @click="onClick">{{ text }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  emits: ["button-click"],
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit("button-click");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  background: #42b983;
+  border: 2px solid #2c3e50;
+  text-transform: uppercase;
+  font-weight: bold;
+  transition: 0.2s;
+  cursor: pointer;
+
+  padding: 5px 8px;
+  margin: 5px;
+}
+
+.btn:hover {
+  transform: scale(1.03, 1.03);
+}
+</style>
